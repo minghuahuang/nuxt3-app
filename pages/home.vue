2473d7e40a05df9a803fa60b8193c01434070639
ming <huangminghua0615@163.com>
2022-12-13 14:16:29 +0800

    core: user useAsyncData

---
+++ pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>home</div>
+	<div v-for="item in listData">{{ item.name }}</div>
+</template>
+
+<script setup>
+	const { data } = await useAsyncData("ssr", () =>
+		$fetch("https://mock.apifox.cn/m1/2077110-0-default/api/nuxt3/fetch")
+	);
+	const listData = data.value.data;
+	console.log(listData);
+</script>
+
+<style scoped></style>
